95fed9c9922b91996eb882b1f4463862c00c0171
Roman Shishko <romanshishko@list.ru>
2023-07-07 10:42:09 +0300

    Update formating

--- app/src/main.c
+++ app/src/main.c
@@ -1,62 +1,62 @@
 /**
-  **************************************************************************
-  * @file     main.c
-  * @brief    main program
-  **************************************************************************
-  *                       Copyright notice & Disclaimer
-  *
-  * The software Board Support Package (BSP) that is made available to
-  * download from Artery official website is the copyrighted work of Artery.
-  * Artery authorizes customers to use, copy, and distribute the BSP
-  * software and its related documentation for the purpose of design and
-  * development in conjunction with Artery microcontrollers. Use of the
-  * software is governed by this copyright notice and the following disclaimer.
-  *
-  * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
-  * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
-  * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
-  * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
-  * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
-  *
-  **************************************************************************
-  */
+ **************************************************************************
+ * @file     main.c
+ * @brief    main program
+ **************************************************************************
+ *                       Copyright notice & Disclaimer
+ *
+ * The software Board Support Package (BSP) that is made available to
+ * download from Artery official website is the copyrighted work of Artery.
+ * Artery authorizes customers to use, copy, and distribute the BSP
+ * software and its related documentation for the purpose of design and
+ * development in conjunction with Artery microcontrollers. Use of the
+ * software is governed by this copyright notice and the following disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
+ * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
+ * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
+ * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
+ * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+ *
+ **************************************************************************
+ */
 
 #include "at32f425_board.h"
 #include "at32f425_clock.h"
 
 /** @addtogroup UTILITIES_examples
-  * @{
-  */
+ * @{
+ */
 
 /** @addtogroup USB_IAP_bootloader
-  * @{
-  */
+ * @{
+ */
 
 /**
-  * @brief  main function.
-  * @param  none
-  * @retval none
-  */
+ * @brief  main function.
+ * @param  none
+ * @retval none
+ */
 int main(void)
 {
-  system_clock_config();
+    system_clock_config();
 
-  nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
+    nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
 
-  at32_board_init();
+    at32_board_init();
 
-  while(1)
-  {
-    at32_led_toggle(LED3);
-    delay_ms(500);
-  }
+    while (1)
+    {
+        at32_led_toggle(LED3);
+        delay_ms(500);
+    }
 }
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-  */
+ * @}
+ */
